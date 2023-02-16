5c05029554f2922dfa0ba676f0285523ae07a8b7
Maycon Kabitschke <mayconkabitschke@gmail.com>
2023-02-16 08:34:25 -0300

    :package: adicionado o slide de fotos

--- js/script.js
+++ js/script.js
@@ -9,6 +9,7 @@ import MenuMobile from './modules/menu-mobile.js';
 import Funcionamento from './modules/funcionamento.js';
 import fetchAnimais from './modules/fetch-animais.js';
 import fetchBitcoin from './modules/fetch-bitcoin.js';
+import Slidenav from './modules/slide.js';
 
 
 const scrollSuave = new ScrollSuave('[data-menu="suave"] a[href^="#"]');
@@ -40,3 +41,7 @@ funcionamento.init();
 
 fetchAnimais('./animaisapi.json', '.numeros-grid');
 fetchBitcoin('https://blockchain.info/ticker', '.btc-preco');
+
+const slide = new Slidenav('.slide', '.slide-wrapper');
+slide.init();
+slide.addControl('.custom-controls');
